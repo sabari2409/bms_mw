18ffed945b238b22930adf115f1effabaff8001e
Sabari Rajeswaran <sabarirajeswaran2409@gmail.com>
2026-03-04 13:31:01 +0530

    Added notes and added lombok annotations to ticketsReqDTO

--- src/main/java/com/scaler/bms/dto/LockSeatResDTO.java
+++ src/main/java/com/scaler/bms/dto/LockSeatResDTO.java
@@ -1,12 +1,14 @@
 package com.scaler.bms.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class LockSeatResDTO {
 
     private String lockId;
--- src/main/java/com/scaler/bms/dto/TicketDetailsDTO.java
+++ src/main/java/com/scaler/bms/dto/TicketDetailsDTO.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
 @Setter
 @NoArgsConstructor
 public class TicketDetailsDTO {
